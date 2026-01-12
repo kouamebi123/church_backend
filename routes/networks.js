dbaece4c90931c71c0dc0ca9e5ae0e38195ef84e
Emmanuel KOUAME <emmanuel.bikouame@gmail.com>
2026-01-12 10:25:53 +0100

    Mise à jour

--- routes/networks.js
+++ routes/networks.js
@@ -23,8 +23,7 @@ const {
   removeCompanion,
   getNetworkCompanions,
   getPublicNetworks,
-  getPublicNetworkGroups,
-  getPublicNetworkDetails
+  getPublicNetworkGroups
 } = require('../controllers/networkController');
 const {
   getNetworkObjective,
@@ -37,7 +36,6 @@ const {
 // Routes publiques pour l'inscription (avant le middleware protect)
 router.get('/public', getPublicNetworks);
 router.get('/public/:id/groups', getPublicNetworkGroups);
-router.get('/public/:id/details', getPublicNetworkDetails);
 
 router.use(protect);
 
